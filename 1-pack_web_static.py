cd286502097a262efef9b507cf57c5b502d29849
Toteya <111172685+Toteya@users.noreply.github.com>
2023-10-05 08:53:41 +0200

    updated output line

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,6 +4,7 @@ module 1-pack_web_static
 """
 from fabric.api import local, settings
 from datetime import datetime
+import os
 
 
 def do_pack():
@@ -19,6 +20,25 @@ def do_pack():
                          str(time.minute),
                          str(time.second)])
 
-    filepath = "versions/web_static_{}.tgz".format(timestamp)
-    local('tar -cvzf {} ./web_static'.format(filepath))
-    return filepath
+    file_path = "versions/web_static_{}.tgz".format(timestamp)
+    source_path = "web_static"
+
+    dir_size = get_dir_size(source_path)
+
+    local('tar -cvzf {} ./web_static'.format(file_path))
+    print("{} packed: {} -> {}Bytes".format(source_path, file_path, dir_size))
+    return file_path
+
+
+def get_dir_size(path):
+    """
+    Get total size in bytes of a directory
+    """
+    size = 0
+    with os.scandir(path) as iterator:
+        for item in iterator:
+            if item.is_file():
+                size += item.stat().st_size
+            elif item.is_dir():
+                size += get_dir_size(item.path)
+    return size
