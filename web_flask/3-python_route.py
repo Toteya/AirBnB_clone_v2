d4c1cd816cbeb853e7557a2c8a45fd380bf0f7e9
Toteya <111172685+Toteya@users.noreply.github.com>
2023-10-27 10:41:09 +0200

    route python with text variable

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+module 1-hello_route
+Starts a Flask Web application listening on 0.0.0.0:5000
+"""
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_hbnb():
+    """Displays a greeting"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays a greeting"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun(text):
+    """C is fun"""
+    text = text.replace("_", " ")
+    return f"C {escape(text)}"
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def python_is_cool(text="is cool"):
+    """Python"""
+    text = text.replace("_", " ")
+    return f"Python {escape(text)}"
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
