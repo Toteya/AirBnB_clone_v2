a9b378b15eae658196c7d98d1e307891c5d43f70
Toteya <111172685+Toteya@users.noreply.github.com>
2023-10-27 10:28:45 +0200

    variable route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+module 1-hello_route
+Starts a Flask Web application listening on 0.0.0.0:5000
+"""
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_hbnb():
+    """Displays a greeting"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays a greeting"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun(text):
+    """C is fun"""
+    return f"C {escape(text)}".replace("_", " ")
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
